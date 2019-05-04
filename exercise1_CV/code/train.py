ac41504697a2c12392e89a183cefad5f01cf140f
bigus <hexed@outlook.com>
2019-05-04 12:43:38 +0200

    used pickle to save/load training/validation errors for plots later on

--- exercise1_CV/code/train.py
+++ exercise1_CV/code/train.py
@@ -1,40 +1,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import torch.optim as optim
+import torch
 
 from model.model import ResNetModel
 from model.data import get_data_loader
 from utils.plot_util import plot_keypoints
-import torch
+
 import argparse
+import pickle
 
 PATH = "model/fuckme.pth"
 OPATH = "model/fuckyou.pth"
-batch_size = 10
-num_epochs = 1000
+batch_size = 5
+num_epochs = 2000
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--c", action = "store_true")
 args = parser.parse_args()
 
-# cuda
+# cuda & model init
 cuda = torch.device('cuda')
 model = ResNetModel(pretrained=False)
-#model.load_state_dict(torch.load(PATH_TO_CKPT))
 model.to(cuda)
 # training loop init
 optimizer = optim.Adam(model.parameters(), lr=1e-4)
 # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=100)
 loss_fn = torch.nn.MSELoss(reduction='elementwise_mean')
 
+train_loss = val_loss = 0
+training_errors = validation_errors = []
+
+# if flag -c is set, continute training from a previous snapshot
 if(args.c):
     model.load_state_dict(torch.load(PATH))
     optimizer.load_state_dict(torch.load(OPATH))
+    with open('training.errors', 'rb') as filehandle:
+        training_errors = pickle.load(filehandle)
+    with open('validation.errors', 'rb') as filehandle:
+        training_errors = pickle.load(filehandle)
 
-
+# get data loaders
 train_loader = get_data_loader(batch_size, is_train=True)
 val_loader = get_data_loader(batch_size, is_train=False)
-train_loss = eval_loss = 0
+
 
 for epoch in range(num_epochs):
     model.train()
@@ -44,9 +53,7 @@ for epoch in range(num_epochs):
         img = img.to(cuda)
         keypoints = keypoints.to(cuda)
         weights = weights.to(cuda)
-
         output = model(img,'')
-
         loss = loss_fn(keypoints, output)*(weights.repeat_interleave(2).float())
         loss = torch.mean(loss)
         train_loss += loss.item()
@@ -55,26 +62,33 @@ for epoch in range(num_epochs):
         if (idx>=5) :
             break
     print("epoch {0}/{1}".format(epoch,num_epochs))
-    print("avg. training loss : {}".format(train_loss/len(train_loader)))
-
+    training_errors.append(train_loss/len(train_loader))
+    print("avg. training loss : {}".format(training_errors[-1]))
+    
     if epoch % 5 == 0: 
         with torch.no_grad():
             model.eval()
-            eval_loss = 0
+            val_loss = 0
             mpjpe = 0
             for idx, (img, keypoints, weights) in enumerate(val_loader):
                  img = img.to(cuda)
                  keypoints = keypoints.to(cuda)
                  weights = weights.to(cuda)
-
                  output = model(img, '')
                  loss = loss_fn(keypoints, output)*(weights.repeat_interleave(2).float())
                  mpjpe += torch.mean(torch.sqrt(loss)).item()
                  eval_loss += torch.mean(loss).item()
                  if(idx>=5) :
                     break 
-            print("evaluation loss : {}".format(eval_loss))
-            print("MPJPE : {}".format(mpjpe/len(val_loader)))
+            print("validation loss : {}".format(val_loss))
+            validation_errors.append(eval_loss)
+            print("MPJPE : {} pixels".format(mpjpe/len(val_loader)))
 
+        # save model state
         torch.save(model.state_dict(), PATH)
         torch.save(optimizer.state_dict(), OPATH)
+        # store training/validation loss as binary data stream
+        with open('results/training.errors', 'wb') as filehandle:  
+            pickle.dump(training_errors, filehandle)
+        with open('results/validation.errors', 'wb') as filehandle:  
+            pickle.dump(validation_errors, filehandle)
