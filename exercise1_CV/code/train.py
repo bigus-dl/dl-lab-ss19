2ed785bbbf5c88f08883fcaf02b9c85b53472316
bigus <hexed@outlook.com>
2019-05-11 10:00:25 +0200

    bug

--- exercise1_CV/code/train.py
+++ exercise1_CV/code/train.py
@@ -98,7 +98,7 @@ for epoch in range(1,args.num_epochs):
         diff = ((output - keypoints)**2).sum(-1)
         print("diff : {}".format(diff.shape))
         print("wieghts : {}".format(weights.shape))
-        loss = (diff * weights).sum(-1)#/ weights.sum(-1)
+        loss = (diff.view(-1,1) * weights).sum(-1)/ weights.sum(-1)
         print("loss : {}".format(loss.shape))
         loss = torch.mean(loss)
         print("lOss : {}".format(loss.shape))
